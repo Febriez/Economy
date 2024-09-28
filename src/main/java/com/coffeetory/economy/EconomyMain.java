20bde2b3e942ddece2b9afb82a987b6e69ecd54b
Febriez <febrie@naver.com>
2024-09-28 13:22:37 +0900

    First Commit

---
+++ src/main/java/com/coffeetory/economy/EconomyMain.java
@@ -0,0 +1,17 @@
+package com.coffeetory.economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
