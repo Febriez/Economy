4900749d1b4b1059591703040385cbe8d834330b
Febriez <febrie@naver.com>
2024-10-03 13:09:03 +0900

    Edit Commit

--- src/main/java/com/coffeetory/economy/command/MainCommand.java
+++ src/main/java/com/coffeetory/economy/command/MainCommand.java
@@ -2,6 +2,7 @@ package com.coffeetory.economy.command;
 
 import com.coffeetory.economy.api.Economy;
 import com.coffeetory.lib.format.MessageFormat;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.command.Command;
@@ -18,17 +19,25 @@ public class MainCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String l, @NotNull String[] args) {
         if (!(sender instanceof Player p)) {
             if (!sender.isOp()) return false;
-            sender.sendMessage(Component.text("§f§l========== [ §a§l돈 명령어 §f§l] =========="));
-            sender.sendMessage(Component.text("/돈 확인 <닉네임>"));
-            sender.sendMessage(Component.text("/돈 설정 <닉네임> <액수>"));
-            sender.sendMessage(Component.text("/돈 전체초기화"));
+            say(sender, "§f§l========== [ §a§l돈 명령어 §f§l] ==========");
+            say(sender, "/돈 확인 <닉네임>");
+            say(sender, "/돈 설정 <닉네임> <액수>");
+            say(sender, "/돈 전체초기화");
             return true;
         }
         Economy eco = new Economy(p.getUniqueId());
         if (args.length == 0) {
-            p.sendMessage(PREFIX.append(Component.text("§e§l" + MessageFormat.m(eco.getBalance()).replaceAll(",", "§f§l,§e§l") + " §f§l원")));
+            sayM(p, "§e§l" + MessageFormat.m(eco.getBalance()).replaceAll(",", "§f§l,§e§l") + " §f§l원");
             return true;
         }
         return false;
+    }
+
+    private void say(@NotNull Audience to, String s) {
+        to.sendMessage(Component.text(s));
+    }
+
+    private void sayM(@NotNull Audience to, String s) {
+        to.sendMessage(PREFIX.append(Component.text(s)));
     }
 }
