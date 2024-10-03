a0510aff95466fee8a38ab817b661eda44834b48
Febriez <febrie@naver.com>
2024-10-03 13:05:09 +0900

    Edit Commit

--- src/main/java/com/coffeetory/economy/api/Economy.java
+++ src/main/java/com/coffeetory/economy/api/Economy.java
@@ -4,23 +4,30 @@ import com.coffeetory.economy.manager.EconomyManager;
 
 import java.util.UUID;
 
+@SuppressWarnings("unused")
 public class Economy {
 
     private static final EconomyManager manager = EconomyManager.getInstance();
 
-    public long getBalance(UUID uuid) {
+    private final UUID uuid;
+
+    public Economy(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public long getBalance() {
         return manager.getData(uuid);
     }
 
-    public boolean has(UUID uuid, long value) {
-        return getBalance(uuid) >= value;
+    public boolean has(long value) {
+        return getBalance() >= value;
     }
 
-    public void subtractBalance(UUID uuid, long value) {
-        addBalance(uuid, -value);
+    public void subtractBalance(long value) {
+        addBalance(-value);
     }
 
-    public void addBalance(UUID uuid, long value) {
+    public void addBalance(long value) {
         manager.addData(uuid, value);
     }
 
--- src/main/java/com/coffeetory/economy/command/MainCommand.java
+++ src/main/java/com/coffeetory/economy/command/MainCommand.java
@@ -1,6 +1,9 @@
 package com.coffeetory.economy.command;
 
+import com.coffeetory.economy.api.Economy;
+import com.coffeetory.lib.format.MessageFormat;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +11,9 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class MainCommand implements CommandExecutor {
+
+    private static final TextComponent PREFIX = Component.text("§f§l[ §a§l소지금 §f§l]§r ");
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String l, @NotNull String[] args) {
         if (!(sender instanceof Player p)) {
@@ -16,6 +22,11 @@ public class MainCommand implements CommandExecutor {
             sender.sendMessage(Component.text("/돈 확인 <닉네임>"));
             sender.sendMessage(Component.text("/돈 설정 <닉네임> <액수>"));
             sender.sendMessage(Component.text("/돈 전체초기화"));
+            return true;
+        }
+        Economy eco = new Economy(p.getUniqueId());
+        if (args.length == 0) {
+            p.sendMessage(PREFIX.append(Component.text("§e§l" + MessageFormat.m(eco.getBalance()).replaceAll(",", "§f§l,§e§l") + " §f§l원")));
             return true;
         }
         return false;
